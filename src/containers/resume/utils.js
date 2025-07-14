6e253fdc40f2939cbbf0223de03808b8b135e788
hab-debug <aiyelabegan40@gmail.com>
2025-07-14 22:43:57 +0100

    first commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Frontend developer",
+        subTitle: "Lagos, Nigeria",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Frontend Engineer",
+        subTitle: "Ilorin, Nigeria ",
+        description:
+          "Web development, Databases, Frontend developement",
+      },
+      {
+        title: "Software Engineer",
+        subTitle: "Ilorin, Nigeria",
+        description:
+          "Creative Direction Project Management, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Ilorin comprehensive High School ",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "kwara State Polytechnic, Ilorin",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "BSc",
+        subTitle: "Al-Hikmah University Ilorin, Nigeria",
+        description:
+          "Bachelor of technology in Physics",
+      },
+    ],
+  };
